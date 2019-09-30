2810b2a4e77965207f9b90da74442dc1272d4084
Carlos Bederian <carlos.bederian@unc.edu.ar>
2019-09-30 15:29:47 -0300

    Move struct context and swtch declaration to context.h

---
+++ kernel/context.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "types.h"
+
+// Saved registers for kernel context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+void swtch(struct context*, struct context*);
--- kernel/proc.h
+++ kernel/proc.h
@@ -1,22 +1,4 @@
-// Saved registers for kernel context switches.
-struct context {
-  uint64 ra;
-  uint64 sp;
-
-  // callee-saved
-  uint64 s0;
-  uint64 s1;
-  uint64 s2;
-  uint64 s3;
-  uint64 s4;
-  uint64 s5;
-  uint64 s6;
-  uint64 s7;
-  uint64 s8;
-  uint64 s9;
-  uint64 s10;
-  uint64 s11;
-};
+#include "context.h"
 
 // Per-CPU state.
 struct cpu {
