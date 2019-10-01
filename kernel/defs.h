7477795091949030559c2ff0e5508808afc08c47
Carlos Bederian <carlos.bederian@unc.edu.ar>
2019-10-01 10:56:53 -0300

    Remove every include in defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -1,26 +1,4 @@
 #pragma once
 
-#include "bio.h"
-#include "console.h"
-#include "context.h"
-#include "cpu.h"
-#include "exec.h"
-#include "file.h"
-#include "fs.h"
-#include "kalloc.h"
-#include "log.h"
-#include "pipe.h"
-#include "plic.h"
-#include "proc.h"
-#include "printf.h"
-#include "ramdisk.h"
-#include "sleeplock.h"
-#include "spinlock.h"
-#include "syscall.h"
-#include "string.h"
-#include "trap.h"
-#include "virtio_disk.h"
-#include "vm.h"
-
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
