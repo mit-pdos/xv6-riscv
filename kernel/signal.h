18c1d6ee30828bac751e39c4a927e4ab3613ade6
Yair Sepunaru <yairsep@gmail.com>
2021-04-19 17:37:14 +0300

    Task 2.1 Complete

---
+++ kernel/signal.h
@@ -0,0 +1,8 @@
+//Task 2.1
+//TODO: Check about includes
+
+#define SIG_DFL 0 /* default signal handling */
+#define SIG_IGN 1 /* ignore signal */
+#define SIGKILL 9
+#define SIGSTOP 17
+#define SIGCONT 19
