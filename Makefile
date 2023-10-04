6e68486f3e39fe38b58a1c5fdf016bb31c5d5028
Seppe <seppe.degryse@telenet.be>
2023-10-04 16:23:34 +0200

    Added multiplewait

--- Makefile
+++ Makefile
@@ -133,6 +133,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_sleep\
+	$U/_multiplewait\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/multiplewait.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Parent %d will start %d processes\n", getpid(), atoi(argv[1]));
+
+    for (int i = 1; i <= atoi(argv[1]); i++)
+    {
+        int rc = fork();
+        if (rc < 0 )
+        {
+            fprintf(2, "Fork failed!\n");
+            exit(1);
+        } else if (rc == 0)
+        {
+            printf("Hello I'am child %d!\n", i);
+        } else 
+        {
+            int status;
+            wait(&status);
+            printf("Child %d finished with status %d!\n", i, status);
+            exit(0);  
+        }
+    }
+
+    exit(0);
+}
