83cfe24af4efa7fe4e209e8768c83ff3f0019bda
geofpwhite <108630660+geofpwhite@users.noreply.github.com>
2023-10-10 16:59:00 -0400

    changed readme

--- plot.py
+++ plot.py
@@ -10,10 +10,10 @@ with open('data.txt', encoding='utf-8') as data:
     for i in data:
         for j in range(50):
             procs[f'proc{j}'].append(
-                procs[f'proc{j}'][len(procs[f'proc{j}'])-1])
+                procs[f'proc{j}'][-1])
         for j in range(50):
             if f' {j} ' in i:
-                procs[f'proc{j}'][len(procs[f'proc{j}'])-1] += 1
+                procs[f'proc{j}'][-1] += 1
                 break
 # df = pd.DataFrame([proc1, proc2],
 #                   columns=['proc1', 'proc2'])
