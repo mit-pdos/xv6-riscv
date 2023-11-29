81fa871aff5718846c84e7ecc81212c0fa822b87
Marcos1411 <103001789+Marcos1411@users.noreply.github.com>
2023-11-28 23:41:18 -0300

    Add files via upload

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,6 +7,9 @@
 #include "syscall.h"
 #include "defs.h"
 
+
+
+
 // Fetch the uint64 at addr from the current process.
 int
 fetchaddr(uint64 addr, uint64 *ip)
@@ -101,6 +104,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_getppid(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +130,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getppid] sys_getppid,
 };
 
 void
@@ -145,3 +150,10 @@ syscall(void)
     p->trapframe->a0 = -1;
   }
 }
+
+
+extern uint64 sys_getppid(void)
+{
+  return myproc()->parent->pid;
+}
+
