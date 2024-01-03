77da6a599ee7f54d8a438094a9f86664e58e0452
rihib <rihib@rihib.dev>
2024-01-03 20:26:04 +0900

    a

--- kernel/fs.c
+++ kernel/fs.c
@@ -685,6 +685,7 @@ static struct inode *namex(char *path, int nameiparent, char *name, int flag) {
     }
   }
 
+  printf("other");
   while ((path = skipelem(path, name)) != 0) {
     ilock(ip);
     if (ip->type != T_DIR) {
