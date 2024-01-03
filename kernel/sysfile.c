01c27789a8c6d47f5b00d4f665e0915a1160929e
rihib <rihib@rihib.dev>
2024-01-04 00:51:38 +0900

    a

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -233,6 +233,7 @@ void register_fullpath_index(char *path, struct inode *ip) {
   // fullpath_index[hash]にフルパス（path）とinodeを登録する
   strcpy(fullpath_index[hash].fullpath, path);
   fullpath_index[hash].ip = ip;
+  fullpath_index[hash].ip->type = T_DIR;
 }
 
 static struct inode *create(char *path, short type, short major, short minor,
