497157a61b548deebba15e0ceb9182248cdfafdb
AmEl <amirelkanov0504@gmail.com>
2024-02-18 21:42:21 +0300

    1 b) done

--- Makefile
+++ Makefile
@@ -132,6 +132,7 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_sum\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/sum.c
@@ -0,0 +1,38 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    const int BUF_SIZE = 20;
+    char buffer[BUF_SIZE];
+
+    // Читаем строку в буфер
+    if (!gets(buffer, BUF_SIZE)) {
+        printf("Invalid arguments!\n");
+        exit(1);
+    }
+
+    // Бежим по прочитанной строке, ищем пробел-разделитель
+    int space_ind = 0;
+    while (space_ind < BUF_SIZE && buffer[++space_ind] != ' ');
+
+    if (space_ind == BUF_SIZE) {
+        printf("Not enough arguments!\n");
+        exit(1);
+    }
+
+    // Заведем наши будущие числа, и будем в них писать
+    char a[space_ind], b[BUF_SIZE - space_ind];
+
+    for (int i = 0; i < space_ind; i++) {
+        a[i] = buffer[i];
+    }
+
+    // Сдвинем метку разделителя направо на 1 и пойдем изучать второе число
+    for (int i = space_ind++; buffer[i] != '\0'; i++) {
+        b[i - space_ind] = buffer[i];
+    }
+
+    printf("%d\n", atoi(a) + atoi(b));
+    exit(0);
+}
