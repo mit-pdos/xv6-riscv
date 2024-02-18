93a448291baebee4997fd02ae377d3ea7425d4d6
AmEl <amirelkanov0504@gmail.com>
2024-02-18 23:41:49 +0300

    1 c) done (unoptimized)

--- Makefile
+++ Makefile
@@ -132,7 +132,8 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
-	$U/_sum\
+	$U/_sumb\
+	$U/_sumc\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/sumc.c
@@ -0,0 +1,39 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int main() {
+    const int BUF_SIZE = 20;
+    char c;
+    int a, b;
+
+    // Я решил сэкономить на памяти: будем работать с одним буфером аккуратненько
+    char buffer[BUF_SIZE];
+
+    int i = 0;
+    // Первый чек на нормальный рид, второй - на переполнение буфера
+    while (read(0, &c, 1) > 0 && i < sizeof buffer) {
+        if (c == '\n' || c == '\r' || c == ' ') break;
+        buffer[i++] = c;
+    }
+    buffer[i] = '\0';
+    a = atoi(buffer);
+
+    // Если все, что мы прочитали - это только одно число
+    if (c != ' ') {
+        printf("Not enough / invalid arguments!\n");
+        exit(1);
+    }
+
+    i = 0;
+    while (read(0, &c, 1) > 0 && i < sizeof buffer) {
+        if (c == '\n' || c == '\r') break;
+        buffer[i++] = c;
+    }
+    buffer[i] = '\0';
+    b = atoi(buffer);
+
+    printf("%d\n", a + b);
+    exit(0);
+}
