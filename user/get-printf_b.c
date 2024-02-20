bfbecc3678fa237f840999f2fa605becf71b90a1
KAlina28 <krylovaalina28@yandex.ru>
2024-02-20 23:56:07 +0000

    мелкие правки и пункт а

--- user/get-printf_b.c
+++ user/get-printf_b.c
@@ -6,41 +6,67 @@
 
 int ptr = 0;
 char buffer_[BUFFER];
-char first[BUFFER], second[BUFFER];
-int ptr_first = 0, ptr_second = 0;
 
-void checking_exist() {
-    if (ptr == BUFFER) {
-        fprintf(2, "can't read second number\n");
+
+void checking_exist(int i) {
+    if ( i ==  1 && ptr == BUFFER) {
+        fprintf(2, "ERROR: can't read first number\n");
+        exit(1);
+    }
+    if ( i ==  2 && ptr == BUFFER) {
+        fprintf(2, "ERROR: can't read second number\n");
         exit(1);
     }
 }
 
 void checking_space(int i){
     if (i == 1 && buffer_[ptr] != ' '){
-        fprintf(2, "enter two numbers separated by a space\n");
+        fprintf(2, "ERROR: need two integer numbers separated by a space\n");
         exit(1);
     }
     if (i == 2 && buffer_[ptr] != '\n'){
-        fprintf(2, "enter two numbers separated by a space\n");
+        fprintf(2, "ERROR: need two integers numbers separated by a space\n");
+        exit(1);
+    }
+}
+
+void checking_correct(int i){
+    if ( i == 2 && !(ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9')) {
+        fprintf(2, "ERROR: the second number was incorrect\n");
+        exit(1);
+    }
+    if ( i == 1 && !(ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9')) {
+        fprintf(2, "ERROR: the first number was incorrect\n");
         exit(1);
     }
 }
 
 
+char first[BUFFER], second[BUFFER];
+int ptr_first = 0, ptr_second = 0;
 
-int main(){
-    gets(buffer_, BUFFER);
+void reading(){
+    checking_exist(1); checking_correct(1);
     while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
         first[ptr_first++] = buffer_[ptr++];
     }
-    checking_exist();
-    checking_space(1);
-    ptr++;
+    checking_exist(2); checking_space(1); ptr++;
+    checking_correct(2);
     while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
         second[ptr_second++] = buffer_[ptr++];
     }
     checking_space(2);
+}
+
+//пункт b
+void get_b(){
+    gets(buffer_, BUFFER);
+    reading();
     printf("%d\n", atoi(first) + atoi(second));
+    exit(0);
+}
+
+int main() {
+    get_b();
     exit(0);
 }
--- user/read-printf_c.c
+++ user/read-printf_c.c
@@ -4,31 +4,61 @@
 
 #define BUFFER 512
 
-char buffer_[BUFFER];
 int ptr = 0;
-char first[BUFFER], second[BUFFER];
-int ptr_first = 0, ptr_second = 0;
-char sym;
+char buffer_[BUFFER];
 
-void checking_exist() {
-    if (ptr == BUFFER) {
-        fprintf(2, "can't read second number\n");
+
+void checking_exist(int i) {
+    if ( i ==  1 && ptr == BUFFER) {
+        fprintf(2, "ERROR: can't read first number\n");
+        exit(1);
+    }
+    if ( i ==  2 && ptr == BUFFER) {
+        fprintf(2, "ERROR: can't read second number\n");
         exit(1);
     }
 }
 
 void checking_space(int i){
     if (i == 1 && buffer_[ptr] != ' '){
-        fprintf(2, "enter two numbers separated by a space\n");
+        fprintf(2, "ERROR: need two integer numbers separated by a space\n");
         exit(1);
     }
     if (i == 2 && buffer_[ptr] != '\n'){
-        fprintf(2, "enter two numbers separated by a space\n");
+        fprintf(2, "ERROR: need two integers numbers separated by a space\n");
         exit(1);
     }
-
 }
 
+void checking_correct(int i){
+    if ( i == 2 && !(ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9')) {
+        fprintf(2, "ERROR: the second number was incorrect\n");
+        exit(1);
+    }
+    if ( i == 1 && !(ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9')) {
+        fprintf(2, "ERROR: the first number was incorrect\n");
+        exit(1);
+    }
+}
+
+
+char first[BUFFER], second[BUFFER];
+int ptr_first = 0, ptr_second = 0;
+
+void reading(){
+    checking_exist(1); checking_correct(1);
+    while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
+        first[ptr_first++] = buffer_[ptr++];
+    }
+    checking_exist(2); checking_space(1); ptr++;
+    checking_correct(2);
+    while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
+        second[ptr_second++] = buffer_[ptr++];
+    }
+    checking_space(2);
+}
+
+char sym;
 
 void pre_reading(){
     while (ptr < BUFFER - 1) {
@@ -43,21 +73,18 @@ void pre_reading(){
     }
 }
 
-int main() {
+
+//пункт c
+void get_c() {
     pre_reading();
     buffer_[ptr] = '\0';
     ptr = 0;
-
-    while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
-        first[ptr_first++] = buffer_[ptr++];
-    }
-    checking_exist();
-    checking_space(1);
-    ptr++;
-    while (ptr < BUFFER && '0' <= buffer_[ptr] && buffer_[ptr] <= '9') {
-        second[ptr_second++] = buffer_[ptr++];
-    }
-    checking_space(2);
+    reading();
     printf("%d\n", atoi(first) + atoi(second));
+    exit(0);
+}
+
+int main() {
+    get_c();
     exit(0);
 }
