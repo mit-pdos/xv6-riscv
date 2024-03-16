44f97937def6494051f7eb31be2548802de25c45
oliver <149434734+oliverdotml@users.noreply.github.com>
2024-03-16 13:35:46 +0000

    refactored main function of xv6 kernel

--- kernel/main.c
+++ kernel/main.c
@@ -7,39 +7,61 @@
 volatile static int started = 0;
 
 // start() jumps here in supervisor mode on all CPUs.
-void
-main()
+void main()
 {
-  if(cpuid() == 0){
+    if (cpuid() == 0) {
+        initializeKernel();
+    } else {
+        waitForKernelInitialization();
+        initializeHart();
+    }
+
+    scheduler();
+}
+
+void initializeKernel()
+{
     consoleinit();
     printfinit();
+    printBootMessage();
+
+    kinit();        // physical page allocator
+    kvminit();      // create kernel page table
+    kvminithart();  // turn on paging
+    procinit();     // process table
+    trapinit();     // trap vectors
+    trapinithart(); // install kernel trap vector
+    plicinit();     // set up interrupt controller
+    plicinithart(); // ask PLIC for device interrupts
+    binit();        // buffer cache
+    iinit();        // inode table
+    fileinit();     // file table
+    virtio_disk_init(); // emulated hard disk
+    userinit();     // first user process
+
+    __sync_synchronize();
+    started = 1;
+}
+
+void waitForKernelInitialization()
+{
+    while (started == 0)
+        ;
+
+    __sync_synchronize();
+}
+
+void initializeHart()
+{
+    printf("hart %d starting\n", cpuid());
+    kvminithart();      // turn on paging
+    trapinithart();     // install kernel trap vector
+    plicinithart();     // ask PLIC for device interrupts
+}
+
+void printBootMessage()
+{
     printf("\n");
     printf("xv6 kernel is booting\n");
     printf("\n");
-    kinit();         // physical page allocator
-    kvminit();       // create kernel page table
-    kvminithart();   // turn on paging
-    procinit();      // process table
-    trapinit();      // trap vectors
-    trapinithart();  // install kernel trap vector
-    plicinit();      // set up interrupt controller
-    plicinithart();  // ask PLIC for device interrupts
-    binit();         // buffer cache
-    iinit();         // inode table
-    fileinit();      // file table
-    virtio_disk_init(); // emulated hard disk
-    userinit();      // first user process
-    __sync_synchronize();
-    started = 1;
-  } else {
-    while(started == 0)
-      ;
-    __sync_synchronize();
-    printf("hart %d starting\n", cpuid());
-    kvminithart();    // turn on paging
-    trapinithart();   // install kernel trap vector
-    plicinithart();   // ask PLIC for device interrupts
-  }
-
-  scheduler();        
 }
