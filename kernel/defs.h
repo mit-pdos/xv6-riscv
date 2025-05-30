f47d8db80d3cd5bd467f1972c96c87d3d611985f
tahsina13 <tahsinahmed217@gmail.com>
2025-05-30 14:41:34 -0400

    add backtrace

--- kernel/defs.h
+++ kernel/defs.h
@@ -80,6 +80,7 @@ int             pipewrite(struct pipe*, uint64, int);
 int            printf(char*, ...) __attribute__ ((format (printf, 1, 2)));
 void            panic(char*) __attribute__((noreturn));
 void            printfinit(void);
+void            backtrace(void);  
 
 // proc.c
 int             cpuid(void);
