5eedb61f8fc4eef941b14b7caf739fe4f74641ea
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-08-05 17:12:48 -0700

    Avoid sleeping on uart_tx_r if panicking

--- kernel/uart.c
+++ kernel/uart.c
@@ -94,7 +94,7 @@ uartputc(int c)
     for(;;)
       ;
   }
-  while(uart_tx_w == uart_tx_r + UART_TX_BUF_SIZE){
+  while(panicking == 0 && uart_tx_w == uart_tx_r + UART_TX_BUF_SIZE){
     // buffer is full.
     // wait for uartstart() to open up space in the buffer.
     sleep(&uart_tx_r, &uart_tx_lock);
