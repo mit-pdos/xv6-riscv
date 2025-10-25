34b3a2a9f6c8b75a46ce2b03c234d6cf9fcb05d9
Rahul-069 <136224663+Rahul-069@users.noreply.github.com>
2025-10-25 10:53:17 +0530

    Add files via upload
    Added modified files

--- kernel/defs.h
+++ kernel/defs.h
@@ -183,3 +183,6 @@ void            virtio_disk_intr(void);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+int             forkwitharg(int);
+uint64           sys_getppid(void);
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,9 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_forkwitharg(void);
+extern uint64 sys_getforkarg(void);
+extern uint64 sys_getppid(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +129,9 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_forkwitharg] sys_forkwitharg,
+[SYS_getforkarg] sys_getforkarg,
+[SYS_getppid] sys_getppid,
 };
 
 void
