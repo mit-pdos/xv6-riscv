58f49bd9a15572795ca917318283284c1811d083
Shyliuli <1658347525@qq.com>
2026-03-14 15:42:35 +0800

    remove unused uartgetc declaration from defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -148,7 +148,7 @@ void            uartinit(void);
 void            uartintr(void);
 void            uartwrite(char [], int);
 void            uartputc_sync(int);
-int             uartgetc(void);
+
 
 // vm.c
 void            kvminit(void);
