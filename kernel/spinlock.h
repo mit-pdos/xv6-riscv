99d8429df7e31107d1ec5aad57254b7d34a68a76
Mohammad Abbas Mehdi <abbasmehdi3727@gmail.com>
2026-04-06 22:23:57 +0530

    final state

--- kernel/spinlock.h
+++
@@ -1,9 +0,0 @@
-// Mutual exclusion lock.
-struct spinlock {
-  uint locked;       // Is the lock held?
-
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
-};
-
