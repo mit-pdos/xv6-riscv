e093abcfe2c4fe4f5853ef08730a2e080418bf17
Bhuvan_M_Acharya <bhuvanm200623@gmail.com>
2026-04-07 10:45:06 +0530

    started with spin locks and table for tags

---
+++ kernel/pfts.c
@@ -0,0 +1,23 @@
+// clang-format off
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "stat.h"
+#include "fs.h"
+#include "file.h"
+#include "defs.h"
+#include "ptfs.h"
+
+// clang-format on
+extern struct {
+  struct spinlock lock;
+  struct inode inode[NINODE];
+} itable;
+
+struct tag_priority tag_table[MAX_TAG_TABLE];
+static int tag_table_count;
+
+static struct spinlock ptfs_lock;
+static int ptfs_lock_ready;
