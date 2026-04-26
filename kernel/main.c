3df1b705a762e446acb9af644aa60171e6cd1845
vini-ai2 <officialvinik@gmail.com>
2026-04-26 21:42:18 +0530

    Update main.c: remove early swap_init to prevent boot panic

--- kernel/main.c
+++ kernel/main.c
@@ -27,6 +27,7 @@ main()
     binit();         // buffer cache
     iinit();         // inode table
     fileinit();      // file table
+    //swap_init();     // swap file initialization
     virtio_disk_init(); // emulated hard disk
     userinit();      // first user process
     __sync_synchronize();
