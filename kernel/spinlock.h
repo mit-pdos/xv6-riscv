1d3bfdcb7c29e6293afda95cc3956744623aa0f3
Vincenzo Romano <vincenzo.romano+git@gmail.com>
2026-05-20 08:03:07 +0200

    Add (spin)lock state values as constants.
    The "inverted" logic allows for faster loops thanks to the use of
    x0/zero register, besides enhanced readability.

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -7,3 +7,8 @@ struct spinlock {
   struct cpu *cpu;   // The cpu holding the lock.
 };
 
+// "Inverted" logic ahead!
+// This is done to get a tiny gain
+// for a tighter spinlock loop
+#define SPINLOCK_HELD 0
+#define SPINLOCK_FREE 1
