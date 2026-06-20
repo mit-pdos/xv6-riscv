154fb248656c3b646654b0953be58922405d737d
Rafia Malik <rafiyam007@gmail.com>
2026-06-20 18:39:23 +0500

    pipe: return -1 on first-byte copyin failure

--- kernel/pipe.c
+++ kernel/pipe.c
@@ -90,8 +90,11 @@ pipewrite(struct pipe *pi, uint64 addr, int n)
       sleep(&pi->nwrite, &pi->lock);
     } else {
       char ch;
-      if (copyin(pr->pagetable, &ch, addr + i, 1) == -1)
+      if(copyin(pr->pagetable, &ch, addr + i, 1) == -1) {
+        if(i == 0)
+          i = -1;
         break;
+      }
       pi->data[pi->nwrite++ % PIPESIZE] = ch;
       i++;
     }
