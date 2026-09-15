fa046142fddd54baed1b0d6eec86b8d725b20e0c
Neima Bravo <nbravo.dev@outlook.com>
2026-09-15 14:37:09 -0600

    ci: use qemu-system-riscv package and fix styling mismatches

--- user/time1.c
+++ user/time1.c
@@ -1,36 +1,37 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]){
-	int pid;
-	int start;
-	int end;
+int
+main(int argc, char *argv[])
+{
+  int pid;
+  int start;
+  int end;
 
-	if(argc < 2){
-		fprintf(2, "usage: time1 command [args...]\n");
-		exit(1);
-	}
+  if (argc < 2) {
+    fprintf(2, "usage: time1 command [args...]\n");
+    exit(1);
+  }
 
-	start = uptime();
+  start = uptime();
 
-	pid = fork();
+  pid = fork();
 
-	if(pid < 0){
-		fprintf(2, "time1:fork failed\n");
-		exit(1);
-	}
+  if (pid < 0) {
+    fprintf(2, "time1:fork failed\n");
+    exit(1);
+  }
 
-	if(pid == 0){
-		exec(argv[1], &argv[1]);
+  if (pid == 0) {
+    exec(argv[1], &argv[1]);
 
-		fprintf(2, "time1: exec failed\n");
-		exit(1);
-	}
+    fprintf(2, "time1: exec failed\n");
+    exit(1);
+  }
 
-	end = uptime();
+  end = uptime();
 
-	printf("elapsed time: %d ticks\n", end - start);
-	
-	exit(0);
+  printf("elapsed time: %d ticks\n", end - start);
+
+  exit(0);
 }
-
