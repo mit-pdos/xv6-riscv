5438c8a329a772209e31578fb11470acaea1e814
Neima Bravo <nbravo.dev@outlook.com>
2026-09-15 13:42:50 -0600

    Added sleep.c file

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char **argv)
+{
+  if(argc != 2){
+    fprintf(2, "usage: sleep <ticks>\n");
+    exit(1);
+  }
+  pause(atoi(argv[1]));
+  exit(0);
+}
+
