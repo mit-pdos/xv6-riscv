2144f70914ce10f30c446664a9de4b713676f02e
Neima Bravo <nbravo.dev@outlook.com>
2026-09-22 04:51:21 +0000

    Complete Task 3 CPU time tracking

--- kernel/proc.c
+++ kernel/proc.c
@@ -123,6 +123,7 @@ allocproc(void)
 
 found:
   p->pid = allocpid();
+  p->cputime = 0;
   p->state = USED;
 
   // Allocate a trapframe page.
--- kernel/trap.c
+++ kernel/trap.c
@@ -82,8 +82,10 @@ usertrap(void)
     kexit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2)
+  if (which_dev == 2) {
+    p->cputime++;
     yield();
+  }
 
   prepare_return();
 
@@ -154,8 +156,10 @@ kerneltrap()
   }
 
   // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2 && myproc() != 0)
+  if (which_dev == 2 && myproc() != 0) {
+    myproc()->cputime++;
     yield();
+  }
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
