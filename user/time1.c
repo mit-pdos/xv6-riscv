27bfc186e9643e051c9772727ed3ce81762b0b14
Neima Bravo <nbravo.dev@outlook.com>
2026-09-22 04:34:07 +0000

    Format time1 command

--- user/time1.c
+++ user/time1.c
@@ -1,11 +1,13 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-  int t1, t2; 
+int
+main(int argc, char *argv[])
+{
+  int t1, t2;
   int p;
 
-  if(argc < 2){
+  if (argc < 2) {
     fprintf(2, "usage: time1 command\n");
 
     exit(1);
@@ -15,13 +17,13 @@ int main(int argc, char *argv[]) {
 
   p = fork();
 
-  if(p < 0){
+  if (p < 0) {
     printf("fork failed!\n");
 
     exit(1);
   }
 
-  if(p == 0){
+  if (p == 0) {
     exec(argv[1], &argv[1]);
 
     printf("exec failed\n");
